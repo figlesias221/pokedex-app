6709db4979344fb2cac0c8d61b4c4bd4b972eff2
Federico Iglesias <figlesias221@gmail.com>
2024-08-15 17:06:45 -0300

    Adds fetch logic for retrival of pokemon list and details

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,1 +1,18 @@
-export const api = {};
+import axios from "axios";
+
+const API_URL = "https://pokeapi.co/api/v2";
+
+export const getPokemonList = async (
+    limit: number = 1000,
+    offset: number = 0
+) => {
+    const response = await axios.get(`${API_URL}/pokemon`, {
+        params: { limit, offset },
+    });
+    return response.data;
+};
+
+export const getPokemonDetails = async (name: string) => {
+    const response = await axios.get(`${API_URL}/pokemon/${name}`);
+    return response.data;
+};
